b1bd8c2dc605e8889e59cedc5cdafe305438629b
chenxiaolei <chenxiaolei91@163.com>
2016-12-13 13:10:09 +0800

    hello

---
+++ src/main/java/com/yonyou/myTest/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.yonyou.myTest;
+
+/**
+ * Created by chenxiaolei on 16/12/13.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld!");
+    }
+}
