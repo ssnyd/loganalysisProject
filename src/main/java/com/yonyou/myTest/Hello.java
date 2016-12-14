3d3040e2a025cc1310548b0ba35f5810a30fc975
chenxiaolei <chenxiaolei91@163.com>
2016-12-14 14:05:42 +0800

    1214更新

--- src/main/java/com/yonyou/myTest/Hello.java
+++ src/main/java/com/yonyou/myTest/Hello.java
@@ -1,37 +1,36 @@
 package com.yonyou.myTest;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.VoidFunction;
+import com.yonyou.utils.DateUtils2;
 
 /**
  * Created by chenxiaolei on 16/12/13.
  */
 public class Hello {
     public static void main(String[] args) {
+        long time = DateUtils2.getTime("[11/Dec/2016:13:57:52 +0800]", 1);
+        System.out.println(time);
+
         //System.out.println(new Date());
         //String todayTime = DateUtils.getTodayTime();
-        //System.out.println(todayTime);
-        SparkConf sconf = new SparkConf()
-                .setAppName("uvipvSpark")
-                .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
-      sconf.setMaster("local[2]");
-      final int i =99;
-        JavaSparkContext sc = new JavaSparkContext(sconf);
-        JavaRDD<String> rdd = sc.textFile("file:////Users/chenxiaolei/rank.txt");
-        rdd.map(new Function<String, String>() {
-            @Override
-            public String call(String s) throws Exception {
-                return s+i;
-            }
-        }).foreach(new VoidFunction<String>() {
-            @Override
-            public void call(String s) throws Exception {
-                System.out.println(s);
-            }
-        });
+      //  //System.out.println(todayTime);
+      //  SparkConf sconf = new SparkConf()
+      //          .setAppName("uvipvSpark")
+      //          .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer");
+      //sconf.setMaster("local[2]");
+      //final int i =99;
+      //  JavaSparkContext sc = new JavaSparkContext(sconf);
+      //  JavaRDD<String> rdd = sc.textFile("file:////Users/chenxiaolei/rank.txt");
+      //  rdd.map(new Function<String, String>() {
+      //      @Override
+      //      public String call(String s) throws Exception {
+      //          return s+i;
+      //      }
+      //  }).foreach(new VoidFunction<String>() {
+      //      @Override
+      //      public void call(String s) throws Exception {
+      //          System.out.println(s);
+      //      }
+      //  });
 
 
     }
